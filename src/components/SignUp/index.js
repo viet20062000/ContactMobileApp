c8661aaf2d33ce7193cac77039688db27917c59d
viet20062000 <57929687+viet20062000@users.noreply.github.com>
2021-07-01 10:39:04 +0700

    update validation for register and setup env variables

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -8,7 +8,7 @@ import {TouchableOpacity} from 'react-native-gesture-handler';
 import {useNavigation} from '@react-navigation/native';
 import {LOGIN} from '../../constants/routeName';
 
-const RegisterComponent = () => {
+const RegisterComponent = ({onSubmit, onChange, errors, form}) => {
   const {navigate} = useNavigation();
   return (
     <Container>
@@ -22,18 +22,50 @@ const RegisterComponent = () => {
         <Text style={styles.Title}>Welcome to RNContacts</Text>
         <Text style={styles.subTitle}>Create a free account</Text>
         <View style={styles.form}>
-          <Input placeholder="Enter Username" label="Username" />
-          <Input placeholder="Enter First Name" label="First Name" />
-          <Input placeholder="Enter Last Name" label="Last Name" />
-          <Input placeholder="Enter E-mail" label="E-mail" />
+          <Input
+            placeholder="Enter Username"
+            label="Username"
+            onChangeText={value => {
+              onChange({name: 'username', value});
+            }}
+            error={errors.username}
+          />
+          <Input
+            placeholder="Enter First Name"
+            label="First Name"
+            onChangeText={value => {
+              onChange({name: 'firstName', value});
+            }}
+            error={errors.firstName}
+          />
+          <Input
+            placeholder="Enter Last Name"
+            label="Last Name"
+            onchangeText={value => {
+              onChange({name: 'lastName', value});
+            }}
+            error={errors.lastName}
+          />
+          <Input
+            placeholder="Enter E-mail"
+            label="E-mail"
+            onChangeText={value => {
+              onChange({name: 'email', value});
+            }}
+            error={errors.email}
+          />
           <Input
             placeholder="Enter Password"
             label="Password"
             secureTextEntry={true}
             icon={<Text>Show</Text>}
             iconPosition="right"
+            onChangeText={value => {
+              onChange({name: 'password', value});
+            }}
+            error={errors.password}
           />
-          <CustomButton title="Submit" primary />
+          <CustomButton title="Submit" onPress={onSubmit} primary />
 
           <View style={styles.registerSection}>
             <Text style={styles.infoText}>Have an account?</Text>
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,8 +1,74 @@
 import React from 'react';
-
+import {useState} from 'react';
 import RegisterComponent from './../../components/SignUp/index';
 const Register = () => {
-  return <RegisterComponent />;
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const onChange = ({name, value}) => {
+    setForm({...form, [name]: value});
+    if (value !== '') {
+      if (name === 'password') {
+        if (value.length < 6) {
+          setErrors(prev => {
+            return {
+              ...prev,
+              [name]: 'The password must longer than 6 characters',
+            };
+          });
+        } else {
+          setErrors(prev => {
+            return {...prev, [name]: null};
+          });
+        }
+      } else {
+        setErrors(prev => {
+          return {...prev, [name]: null};
+        });
+      }
+    } else {
+      setErrors(prev => {
+        return {...prev, [name]: 'This field is required'};
+      });
+    }
+  };
+
+  const onSubmit = () => {
+    console.log('form=>>', form);
+    if (!form.username) {
+      setErrors(prev => {
+        return {...prev, username: 'Please add the username'};
+      });
+    }
+    if (!form.firstName) {
+      setErrors(prev => {
+        return {...prev, firstName: 'Please add the first name'};
+      });
+    }
+    if (!form.lastName) {
+      setErrors(prev => {
+        return {...prev, lastName: 'Please add the username'};
+      });
+    }
+    if (!form.email) {
+      setErrors(prev => {
+        return {...prev, email: 'Please add the email'};
+      });
+    }
+    if (!form.password) {
+      setErrors(prev => {
+        return {...prev, password: 'Please add the password'};
+      });
+    }
+  };
+  return (
+    <RegisterComponent
+      onSubmit={onSubmit}
+      onChange={onChange}
+      errors={errors}
+      form={form}
+    />
+  );
 };
 
 export default Register;
